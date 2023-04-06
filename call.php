cebbf205f98e1c850422c8daec84ca5be0060955
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-06 14:03:06 +0800

    Add files via upload

---
+++ call.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-Type:text/html; charset=utf-8");
+/*
+$command ="C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe ram.py ";
+$output = exec($command);
+print($output);
+*/
+function ram() {
+	$command ="C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/ram.py ";
+	$output = exec($command);
+	print($output);
+}
+function cpu() {
+	$command ="C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/cpu.py ";
+	$output = exec($command);
+	print($output);
+}
+
+if (isset($_GET['ram'])) {
+	ram();
+}
+if (isset($_GET['cpu'])) {
+	cpu();
+}
+?>
