b804218b0299b8d55084dcc19d60a2fc5994e8a7
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-07 08:33:42 +0800

    Update call.php

--- call.php
+++ call.php
@@ -1,16 +1,22 @@
 <?php
 
-
+/*
+function ram() {
+	$command = "C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/ram.py";
+	$output = exec($command);
+	echo($output);
+}
+*/
 function ram() {	
-	$command ="tasklist /fo list";
+	$command = "tasklist /fo list";
 	$output = shell_exec($command);
 	$result = str_replace("\n","<br>",$output);
 	echo iconv("big5","UTF-8//IGNORE", $result);	
 }
 function cpu() {
-	$command ="C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/cpu.py ";
+	$command = "C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/cpu.py ";
 	$output = exec($command);
-	print($output);
+	echo($output);
 }
 
 
