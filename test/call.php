1f2ab1a35d378574c4213a5f4cd15106cb8af931
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-14 16:01:27 +0800

    Add files via upload

---
+++ test/call.php
@@ -0,0 +1,28 @@
+<?php
+// header("Content-Type:text/html; charset=utf-8");
+/*
+$command ="C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe call/ram.py ";
+$output = exec($command);
+print($output);
+*/
+
+/*
+	$locale = 'zh_CN.UTF-8';
+	setlocale(LC_ALL, $locale);
+	putenv('LC_ALL='.$locale);
+*/
+
+// taskkill /s 192.168.0.8 /u user /p 00000000 /f /im chrome.exe
+
+if (isset($_POST['submit'])) {
+	$ip = $_POST['ip'];
+	$user = $_POST['user'];
+	$pw = $_POST['pw'];
+	$command ="tasklist /s $ip /u $user /p $pw /fo list";
+	echo 'IP: '.$ip.'<br>';
+	$output = shell_exec($command);
+	$result = str_replace("\n","<br>",$output);
+	echo iconv("big5","UTF-8//IGNORE", $result);
+}
+
+?>
