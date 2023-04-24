cfa0ce5cbf3f57a0513753e814b38af9b55431a4
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-24 17:19:55 +0800

    Update csvprocess.py

--- csvprocess.py
+++ csvprocess.py
@@ -6,11 +6,6 @@ ram_str = df[df.columns[-1]].str.strip('K')
 ram_str = ram_str.str.replace(',','',1)
 ram = ram_str.astype('int')
 
-'''
-test = filter(str.isdigit,a)
-int("".join(list(test)))
-'''
-
 df.insert(2,'RAM',ram)
 df.sort_values(by=['RAM'], ascending=True, inplace=True)
 df.to_csv('info\\test.csv', index=False, encoding='big5')
