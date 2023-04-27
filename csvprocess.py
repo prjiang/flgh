70d69b94ae6276ed495b948293506fa0ad414e71
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-27 09:37:05 +0800

    Update csvprocess.py

--- csvprocess.py
+++ csvprocess.py
@@ -3,11 +3,11 @@ import pandas as pd
 ''' txt to csv
 import csv
 
-csvfile = open("info/test.csv","w",newline="")
+csvfile = open("info/ram.csv","w",newline="")
 writer = csv.writer(csvfile)
 csvrow = []
 
-f = open("info/test.txt","r")
+f = open("info/ram.txt","r")
 for line in f:
     csvrow = line.split()
     writer.writerow(csvrow)
@@ -16,7 +16,7 @@ f.close()
 csvfile.close()
 '''
 
-df = pd.read_csv('info\\test.txt', encoding = 'big5')
+df = pd.read_csv('info\\ram.txt', encoding = 'big5')
 
 ram_str = df[df.columns[-1]].str.strip('K')
 ram_str = ram_str.str.replace(',','',1)
@@ -29,4 +29,4 @@ df = df.drop(columns=['工作階段名稱','工作階段 #','RAM使用量'])
 columns = ['映像名稱']
 df = df.drop_duplicates(subset=columns, keep='first')
 
-df.to_csv('info\\test.csv', index=False, encoding='big5')
+df.to_csv('info\\ram.csv', index=False, encoding='big5')
