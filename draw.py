693edfaf77170e4c2ee502bf9dc2bfe42bf6d049
prjiang <59597823+prjiang@users.noreply.github.com>
2023-04-27 14:23:02 +0800

    Update draw.py

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ df = pd.read_csv('info/ram.csv', encoding='big5')
 
 df = df.drop(columns=['PID'])
 
-dataset = df.head(10)
+dataset = df.loc[:].head(10) # .loc[:] -> avoid getting the SettingWithCopyWarning
 dataset.sort_values(by=['RAM'], ascending=True, inplace=True)
 dataset.set_index('映像名稱', inplace=True)
 
